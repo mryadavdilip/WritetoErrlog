c997cab2e54bceb7a397a17df71bfd76b6b288de
Dilip Yadav <mryadavdilip@gmail.com>
2022-06-13 18:44:30 +0530

    last commit

--- WritetoErrlog/Form1.Designer.vb
+++ WritetoErrlog/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class WritetoErrlog
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,33 +22,69 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
-        Me.Button1 = New System.Windows.Forms.Button()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(WritetoErrlog))
+        Me.createFile_btn = New System.Windows.Forms.Button()
+        Me.StatusLable = New System.Windows.Forms.Label()
+        Me.MessageBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'Button1
+        'createFile_btn
         '
-        Me.Button1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
-        Me.Button1.Cursor = System.Windows.Forms.Cursors.Cross
-        Me.Button1.Location = New System.Drawing.Point(317, 326)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(177, 82)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = False
+        Me.createFile_btn.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.createFile_btn.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.createFile_btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.createFile_btn.Location = New System.Drawing.Point(317, 263)
+        Me.createFile_btn.Name = "createFile_btn"
+        Me.createFile_btn.Size = New System.Drawing.Size(177, 82)
+        Me.createFile_btn.TabIndex = 0
+        Me.createFile_btn.Text = "Add Message"
+        Me.createFile_btn.UseVisualStyleBackColor = False
         '
-        'Form1
+        'StatusLable
+        '
+        Me.StatusLable.AutoSize = True
+        Me.StatusLable.Location = New System.Drawing.Point(313, 376)
+        Me.StatusLable.Name = "StatusLable"
+        Me.StatusLable.Size = New System.Drawing.Size(0, 20)
+        Me.StatusLable.TabIndex = 1
+        '
+        'MessageBox
+        '
+        Me.MessageBox.Location = New System.Drawing.Point(317, 126)
+        Me.MessageBox.Name = "MessageBox"
+        Me.MessageBox.Size = New System.Drawing.Size(177, 26)
+        Me.MessageBox.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(313, 103)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(115, 20)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Write Message"
+        '
+        'WritetoErrlog
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MessageBox)
+        Me.Controls.Add(Me.StatusLable)
+        Me.Controls.Add(Me.createFile_btn)
+        Me.Cursor = System.Windows.Forms.Cursors.Arrow
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-        Me.Name = "Form1"
+        Me.Name = "WritetoErrlog"
         Me.Text = "WritetoErrlog"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Button1 As Button
+    Friend WithEvents createFile_btn As Button
+    Friend WithEvents StatusLable As Label
+    Friend WithEvents MessageBox As TextBox
+    Friend WithEvents Label1 As Label
 End Class
--- WritetoErrlog/Form1.vb
+++ WritetoErrlog/Form1.vb
@@ -1,5 +1,28 @@
-﻿Public Class Form1
+﻿Public Class WritetoErrlog
+    Dim dir As String = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+    Dim dateTime As DateTime = New DateTime()
+    Dim message
+    Dim status As Boolean
+    Sub Input(ByRef msg As String, ByRef status As Boolean)
+        Dim file As System.IO.StreamWriter
+        Try
+            file = My.Computer.FileSystem.OpenTextFileWriter(dir + "/WritetoErrlogMessages.txt", True)
+            file.WriteLine("(" + DateTime.Now.ToString + "): " + msg)
+            file.Close()
+            status = True
+        Catch ex As Exception
+            status = False
+        Finally
+            StatusLable.Text = status.ToString
+        End Try
+    End Sub
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub createFile_btn_Click(sender As Object, e As EventArgs) Handles createFile_btn.Click
+        message = MessageBox.Text
+        Input(message, status)
     End Sub
 End Class
